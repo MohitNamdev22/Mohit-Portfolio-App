0201dc9582497a22f1d15e3864165abca11f6a75
Mohit Namdev <namdevmohit0@gmail.com>
2024-04-23 00:34:12 +0530

    fixed the footer box

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -33,39 +33,42 @@ const Footer = () => {
     })
   }
   
-  return (
-    <>
-      <h2 className="head-text">Take a coffee & chat with me</h2>
-    <div className="app__footer-cards">
-      <div className="app__footer-card">
-        <img src={images.email} alt="email" />
-        <a href="mailto:namdevmohit0@gmail.com" className='p-text'>namdevmohit0@gmail.com</a>
-      </div>
-      <div className="app__footer-card">
-        <img src={images.mobile} alt="mobile" />
-        <a href="tel:+91 8770847520" className='p-text'>8770847520</a>
-      </div>
-    </div>
-
-    {!isFormSubmitted ?
-    <div className="app__footer-form app__flex">
-      <div className="app__flex">
-        <input type="text" className='p-text' placeholder='Your Name' name='name' value={name} onChange={handleChangeInput} />
-      </div>
-      <div className="app__flex">
-        <input type="email" className='p-text' placeholder='Your Email' name='email' value={email} onChange={handleChangeInput} />
-      </div>
-      <div>
-        <textarea name="" id="" className='p-text' placeholder='Your Message' value={message} name="message" onChange={handleChangeInput} ></textarea>
+    return (
+      <>
+        <h2 className="head-text">Take a coffee & chat with me</h2>
+        <div className="main-box">
+      <div className="app__footer-cards">
+        <div className="app__footer-card">
+          <img src={images.email} alt="email" />
+          <a href="mailto:namdevmohit0@gmail.com" className='p-text'>namdevmohit0@gmail.com</a>
         </div>
-        <button type='button' className='p-text' onClick={handleSubmit}>{ loading ?'Sending':'Send Message'}</button>
-    </div>
-    : <div>
-      <h3 className='head-text'>Thank You for geting in touch!</h3>
-    </div>}
-    </>
-  )
-}
+        <div className="app__footer-card">
+          <img src={images.mobile} alt="mobile" />
+          <a href="tel:+91 8770847520" className='p-text'>8770847520</a>
+        </div>
+      </div>
+  
+      {!isFormSubmitted ?
+      <div className="app__footer-form app__flex">
+        <div className="app__flex">
+          <input type="text" className='p-text' placeholder='Your Name' name='name' value={name} onChange={handleChangeInput} />
+        </div>
+        <div className="app__flex">
+          <input type="email" className='p-text' placeholder='Your Email' name='email' value={email} onChange={handleChangeInput} />
+        </div>
+        <div>
+          <textarea name="" id="" className='p-text' placeholder='Your Message' value={message} name="message" onChange={handleChangeInput} ></textarea>
+          </div>
+          <button type='button' className='p-text' onClick={handleSubmit}>{ loading ?'Sending':'Send Message'}</button>
+      </div>
+      : <div>
+        <h3 className='head-text'>Thank You for geting in touch!</h3>
+      </div>}
+      </div>
+      </>
+    )
+  }
+  
 
 export default AppWrap(
   MotionWrap(Footer,'app__footer'),
