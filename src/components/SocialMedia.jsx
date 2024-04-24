e672b98a59d14659409edb1aa92eef4fa57c623a
Mohit Namdev <namdevmohit0@gmail.com>
2024-04-25 00:45:38 +0530

    added social media links

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -6,13 +6,19 @@ const SocialMedia = () => {
   return (
     <div className='app__social'>
         <div>
+          <a href="https://www.linkedin.com/in/mohitnamdev/" target='_blank'>
             <BsLinkedin/>
+            </a>
         </div>
         <div>
+          <a href="https://github.com/MohitNamdev22" target="_blank" rel="noopener noreferrer">
             <FaGithub/>
+            </a>
         </div>
         <div>
+          <a href="https://www.instagram.com/mohit_namdev03/" target='_blank'>
             <BsInstagram/>
+            </a>
         </div>
       
     </div>
